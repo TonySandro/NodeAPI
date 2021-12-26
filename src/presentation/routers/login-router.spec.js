2eb654a81f46f572922f97094b340c1b6b37f0b5
devTiconnecte <85088970+TonySandro@users.noreply.github.com>
2021-12-26 20:33:24 -0300

    Remove duplicated code and ensure returns 500 if invalid httpRequests

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,13 @@
 class LoginRouter {
   route(httpRequest) {
-    if (!httpRequest.body.email || !httpRequest.body.password) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500,
+      };
+    }
+
+    const { email, password } = httpRequest.body;
+    if (!email || !password) {
       return {
         statusCode: 400,
       };
@@ -9,7 +16,7 @@ class LoginRouter {
 }
 
 describe("Login Router", () => {
-  test("Sgould return 400 if no email is provided", () => {
+  test("Should return 400 if no email is provided", () => {
     const sut = new LoginRouter();
     const httpRequest = {
       body: {
@@ -20,7 +27,7 @@ describe("Login Router", () => {
     expect(httpResponse.statusCode).toBe(400);
   });
 
-  test("Sgould return 400 if no password is provided", () => {
+  test("Should return 400 if no password is provided", () => {
     const sut = new LoginRouter();
     const httpRequest = {
       body: {
@@ -29,5 +36,18 @@ describe("Login Router", () => {
     };
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+  });
+
+  test("Should return 500 if no hettpRequest is provided", () => {
+    const sut = new LoginRouter();
+    const httpResponse = sut.route();
+    expect(httpResponse.statusCode).toBe(500);
+  });
+
+  test("Should return 500 if hettpRequest has no body", () => {
+    const sut = new LoginRouter();
+    const httpRequest = {};
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
   });
 });
