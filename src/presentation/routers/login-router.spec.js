bf0c0f62654546246e175da0b2d086548f0eebde
devTiconnecte <85088970+TonySandro@users.noreply.github.com>
2021-12-27 16:42:55 -0300

    sut creation to a factory helper method (makeSut)

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,13 +1,22 @@
 const LoginRouter = require("./login-router");
 const MissingParamError = require("../helpers/missing-param-error");
 
+/* 
+Designer pattern Factory. 
+Para evitar que quando mude a instancia de um objeto, 
+afete todos os lugares que usavam o mesmo.
+*/
+const makeSut = () => {
+  return new LoginRouter();
+};
+
 describe("Login Router", () => {
   test("Should return 400 if no email is provided", () => {
-    const sut = new LoginRouter();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         password: "anyPassword",
-      }, 
+      },
     };
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
@@ -15,7 +24,7 @@ describe("Login Router", () => {
   });
 
   test("Should return 400 if no password is provided", () => {
-    const sut = new LoginRouter();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         email: "any_email",
@@ -27,15 +36,22 @@ describe("Login Router", () => {
   });
 
   test("Should return 500 if no hettpRequest is provided", () => {
-    const sut = new LoginRouter();
+    const sut = makeSut();
     const httpResponse = sut.route();
     expect(httpResponse.statusCode).toBe(500);
   });
 
   test("Should return 500 if hettpRequest has no body", () => {
-    const sut = new LoginRouter();
+    const sut = makeSut();
     const httpRequest = {};
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(500);
   });
+
+  // test("Should call AuthUseCase with correct params", () => {
+  //   const sut = makeSut();
+  //   const httpRequest = {};
+  //   const httpResponse = sut.route(httpRequest);
+  //   expect(httpResponse.statusCode).toBe(500);
+  // });
 });
