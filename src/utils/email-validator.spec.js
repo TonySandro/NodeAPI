9d85e71d8bf25478436b83d1366a7e7b2a620eb7
devTiconnecte <85088970+TonySandro@users.noreply.github.com>
2022-01-01 21:36:20 -0300

    ensure EmailValidator calls with correct email

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -16,13 +16,18 @@ describe("Email validator", () => {
     const isEmailValid = sut.isValid("valid@email.com");
     expect(isEmailValid).toBe(true);
   });
-});
 
-describe("Email validator", () => {
   test("Should return false if validator returns false", () => {
     validator.isEmailValid = false;
     const sut = makeSut();
     const isEmailValid = sut.isValid("valid@email.com");
     expect(isEmailValid).toBe(false);
+  });
+
+  test("Should call validator with correct email", () => {
+    validator.isEmailValid = false;
+    const sut = makeSut();
+    sut.isValid("anyEmail@email.com");
+    expect(validator.email).toBe("anyEmail@email.com");
   });
 });
