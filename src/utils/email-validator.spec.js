25f8ac7364f0c65c0b09c7cfc16bc0fa2d2a71c9
devTiconnecte <85088970+TonySandro@users.noreply.github.com>
2022-01-01 21:29:37 -0300

    create sut factory metod

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -6,9 +6,13 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator();
+};
+
 describe("Email validator", () => {
   test("Should return true if validator returns true", () => {
-    const sut = new EmailValidator();
+    const sut = makeSut();
     const isEmailValid = sut.isValid("valid@email.com");
     expect(isEmailValid).toBe(true);
   });
@@ -17,7 +21,7 @@ describe("Email validator", () => {
 describe("Email validator", () => {
   test("Should return false if validator returns false", () => {
     validator.isEmailValid = false;
-    const sut = new EmailValidator();
+    const sut = makeSut();
     const isEmailValid = sut.isValid("valid@email.com");
     expect(isEmailValid).toBe(false);
   });
